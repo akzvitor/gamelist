6c48a0e833cd3938a9a81eff7a9ec03ec40f0769
akzvitor <vitor_eterno@outlook.com>
2023-11-18 14:45:05 -0300

    configuring to get games by lists

--- src/main/java/com/akzvitor/dslist/services/GameService.java
+++ src/main/java/com/akzvitor/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.akzvitor.dslist.dto.GameDTO;
 import com.akzvitor.dslist.dto.GameMinDTO;
 import com.akzvitor.dslist.entities.Game;
+import com.akzvitor.dslist.projections.GameMinProjection;
 import com.akzvitor.dslist.repositories.GameRepository;
 
 @Service
@@ -28,6 +29,13 @@ public class GameService {
 	public GameDTO findById(Long id) {
 		Game result = gameRepository.findById(id).get();
 		GameDTO dto = new GameDTO(result);
+		return dto;
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId) {
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		return dto;
 	}
 }
